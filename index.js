0f248af7ebd6e76e11d19972bdfada546f38c275
Ponabarnaa <ponabarnaj@gmail.com>
2026-01-28 19:49:25 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import {connectDB} from "./Db/db.js";
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import userRoute from "./Routes/userRoutes.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+//middleware
+app.use(express.json())
+app.use(cors())
+connectDB()
+// http://localhost:5000/api/user/signup
+app.use('/api/users',userRoute)
+dotenv.config();
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`);
+})
