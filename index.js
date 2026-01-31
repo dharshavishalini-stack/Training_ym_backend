2918cc1dd3be8827564154d8d0642ba68db2f464
Vishalini Dharsha <dharshavishalini@gmail.com>
2026-01-31 09:05:43 +0530

    Added auth model and utils, updated package files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import userRoute from "./Routes/userRoutes.js";
+import authUserRoute from "./Routes/authUserRoutes.js";
 dotenv.config();
 
 const app = express();
@@ -11,14 +12,31 @@ const PORT = process.env.PORT || 5000
 //middleware
 app.use(express.json())
 app.use(cors())
+//connectivity 
 connectDB()
+
 // http://localhost:5000/api/user/signup
 // http://localhost:5000/api/user/getusers
 // http://localhost:5000/api/user/updatepassword/:id
 
 app.use('/api/users',userRoute)
+app.use('/api/authusers',authUserRoute)
 dotenv.config();
 
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
 })
+
+
+
+
+
+
+
+
+
+
+//http://localhost:5000/api/authusers/authsignup
+//http://localhost:5000/api/authusers/authlogin
+//http://localhost:5000/api/authusers/profile
+//http://localhost:5000/api/authusers/adminprofile
